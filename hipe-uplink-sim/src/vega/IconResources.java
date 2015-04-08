92588bb0c89d4c37aec8c4ff41e0c60533997d97
javier.arenas@gmail.com <javier.arenas@gmail.com>
2015-04-08 13:20:22 +0200

    IconResources

---
+++ hipe-uplink-sim/src/vega/IconResources.java
@@ -0,0 +1,27 @@
+package vega;
+
+public class IconResources {
+	public static String HUS_ICON_NOROOT="vega/hus.png"; 
+	public static String GIT_ICON_NOROOT="vega/Git-Icon.png";
+	public static String XML_ICON_NOROOT="vega/xml.png";
+	public static String HUS_ICON="/vega/hus.png"; 
+	public static String GIT_ICON="/vega/Git-Icon.png";
+	public static String XML_ICON="/vega/xml.png";
+	public static String POINTING_ICON_NOROOT="vega/pointing.png";
+	public static String POINTING_ICON="/vega/pointing.png";
+	public static String PTR_ICON_NOROOT="vega/ptr.png";
+	public static String PTR_ICON="/vega/ptr.png";
+	public static String PTSL_ICON_NOROOT="vega/ptsl.png";
+	public static String PTSL_ICON="/vega/ptsl.png";
+	public static String PDFM_ICON_NOROOT="vega/pdfm.png";
+	public static String PDFM_ICON="/vega/pdfm.png";
+	public static String POR_ICON_NOROOT="vega/por.png";
+	public static String POR_ICON="/vega/por.png";
+	public static String PORG_ICON_NOROOT="vega/porg.png";
+	public static String PORG_ICON="/vega/porg.png";
+	public static String FECS_ICON_NOROOT="vega/fecs.png";
+	public static String FECS_ICON="/vega/fecs.png";
+	public static String ITL_ICON_NOROOT="vega/itl.png";
+	public static String ITL_ICON="/vega/itl.png";
+
+}
